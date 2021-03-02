94311edd0f0b4472e923a99242b132222741c8ef
theo-brown <7982453+theo-brown@users.noreply.github.com>
2021-03-02 14:38:21 +0000

    Add json for match_config

--- webapp.py
+++ webapp.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/match_config.json')
+def match_config():
+    with open("/home/tab53/programs/matchbot/get5/configs/match_config.json") as f:
+        config = json.load(f)
+    return config
+
+if __name__ == '__main__':
+    app.run(debug=True)
