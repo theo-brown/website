972a446960570fb86016cb5744e7c931411925ba
theo-brown <7982453+theo-brown@users.noreply.github.com>
2021-04-03 18:57:47 +0100

    Add draft minecraft page

--- webapp.py
+++ webapp.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-import json
+from flask import Flask, render_template, json, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,11 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/match_config.json')
+@app.route('/match_config')
 def match_config():
-    with open("/home/tab53/programs/matchbot/get5/configs/match_config.json") as f:
+    with open("/home/theo/Documents/projects/discord/matchbot/get5/configs/match_config.json") as f:
         config = json.load(f)
-    return config
+    return jsonify(config)
+
+MinecraftButtonText = "Start"
+@app.route('/minecraft', methods=["POST", "GET"])
+def minecraft():
+    global MinecraftButtonText
+    if request.method == "POST":
+        if MinecraftButtonText == "Start":
+            MinecraftButtonText = "Stop"
+        else:
+            MinecraftButtonText = "Start"
+    return render_template('minecraft.html', MinecraftButtonText=MinecraftButtonText)
 
 if __name__ == '__main__':
     app.run(debug=True)
