3ece80d6d0eaea6815d49c85878f2030c28f070b
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-26 09:49:05 +0100

    Add Python setup; comment unused pages

--- website.py
+++ website.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, jsonify, request
+from flask import Flask, render_template, json, jsonify, request, redirect
 import minecraft
 
 app = Flask(__name__)
@@ -7,26 +7,31 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/match_config')
-def match_config_page():
-    with open("/home/tab53/programs/matchbot/get5/configs/match_config.json") as f:
-        config = json.load(f)
-    return jsonify(config)
+# @app.route('/match_config')
+# def match_config_page():
+#     with open("/home/tab53/programs/matchbot/get5/configs/match_config.json") as f:
+#         config = json.load(f)
+#     return jsonify(config)
+#
+# @app.route('/minecraft', methods=["POST", "GET"])
+# def minecraft_page():
+#     MinecraftStatusText = minecraft.status_text()
+#     MinecraftButtonText = "Stop" if minecraft.is_running() else "Start"
+#
+#     if request.method == "POST":
+#         if MinecraftButtonText == "Start":
+#             minecraft.start()
+#         else:
+#             minecraft.stop()
+#         MinecraftStatusText = minecraft.status_text()
+#         MinecraftButtonText = "Stop" if minecraft.is_running() else "Start"
+#
+#     return render_template('minecraft.html', MinecraftButtonText=MinecraftButtonText, MinecraftStatusText=MinecraftStatusText)
 
-@app.route('/minecraft', methods=["POST", "GET"])
-def minecraft_page():
-    MinecraftStatusText = minecraft.status_text()
-    MinecraftButtonText = "Stop" if minecraft.is_running() else "Start"
+@app.route('/setup/python')
+def setup_python_page():
+    return redirect('https://raw.githubusercontent.com/theo-brown/setup/main/setup-python.sh')
 
-    if request.method == "POST":
-        if MinecraftButtonText == "Start":
-            minecraft.start()
-        else:
-            minecraft.stop()
-        MinecraftStatusText = minecraft.status_text()
-        MinecraftButtonText = "Stop" if minecraft.is_running() else "Start"
-
-    return render_template('minecraft.html', MinecraftButtonText=MinecraftButtonText, MinecraftStatusText=MinecraftStatusText)
 
 if __name__ == '__main__':
     app.run(debug=True)
