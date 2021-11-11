6632c73c75b229c081b6d51742ab95eb22c918f8
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-11-11 15:02:07 +0000

    Add comments

--- webhooks/website-update.sh
+++ webhooks/website-update.sh
@@ -1,4 +1,9 @@
+echo "Stopping website service..."
 systemctl --user stop website
+echo "Discarding local changes..."
 git reset --hard
+echo "Pulling remote changes..."
 git pull
+echo "Starting website service..."
 systemctl --user start website
+echo "Done."
