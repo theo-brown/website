793713133b3e6c89cc97d14c79ad336bd10538e7
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-11-11 14:54:48 +0000

    Revert to real website-update.sh

--- webhooks/website-update.sh
+++ webhooks/website-update.sh
@@ -1,5 +1,4 @@
-#systemctl --user stop website
-#git reset --hard
-#git pull
-#systemctl --user start website
-echo "hit" >> counter.txt
+systemctl --user stop website
+git reset --hard
+git pull
+systemctl --user start website
