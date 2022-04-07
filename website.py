53a0d08f1f8d6605f8acb1b687aa71aa419c56ab
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2022-04-07 15:59:35 +0100

    Update website.py to point to local files; tidy up webhooks

--- website.py
+++ website.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, jsonify
+from flask import Flask, render_template, redirect, request, send_file, jsonify
 from os import getenv
 from dotenv import load_dotenv
 import hmac
@@ -10,6 +10,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
+# PAGES
 
 @app.route('/')
 def index_page():
@@ -18,26 +19,25 @@ def index_page():
 
 @app.route('/cv')
 def cv_page():
-    return redirect('https://raw.githubusercontent.com/theo-brown/cv/main/cv.pdf')
+    return send_file('static/files/cv/cv.pdf')
 
 
-@app.route('/setup/python')
-def setup_python_page():
-    return redirect('https://raw.githubusercontent.com/theo-brown/setup/main/setup-python.sh')
+@app.route('/setup/conda')
+def setup_conda_page():
+    return send_file('static/files/setup/conda-setup.sh')
 
 
-@app.route('/webhooks/website-update', methods=['POST'])
-def website_update_webhook():
-    if verify_github_webhook(request, getenv('WEBSITE_UPDATE_WEBHOOK_SECRET')):
-        process = subprocess.run(["bash", "webhooks/website-update.sh"], cwd=getenv('WEBSITE_DIR'))
-        if process.returncode == 0:
-            return jsonify({'message': 'success'}), 200
-        else:
-            return jsonify({'message': 'failure'}), 500
-    else:
-        return jsonify({'message': 'failure'}), 404
+@app.route('/setup/ssh')
+def setup_ssh_page():
+    return send_file('static/files/setup/ssh-setup.sh')
 
 
+@app.route('/setup/docker')
+def setup_docker_page():
+    return send_file('static/files/setup/docker-setup.sh')
+
+
+# WEBHOOKS
 def verify_signature(request_object, signature, secret):
     hashed_secret = hmac.new(secret.encode(),
                              msg=request_object.get_data(),
@@ -51,6 +51,38 @@ def verify_github_webhook(request_object, secret):
         return False
     sha_type, signature = header_signature.split('=')
     return verify_signature(request_object, signature, secret)
+
+
+def run_webhook(request, secret, commands):
+   if verify_github_webhook(request, secret):
+        process = subprocess.run(commands, cwd=getenv('WEBSITE_DIR'))
+        if process.returncode == 0:
+            return jsonify({'message': 'success'}), 200
+        else:
+            return jsonify({'message': 'failure'}), 500
+    else:
+        return jsonify({'message': 'failure'}), 404
+
+
+@app.route('/webhooks/setup-update', methods=['POST'])
+def setup_update_webhook():
+    return run_webhook(request,
+                       getenv('SETUP_UPDATE_WEBHOOK_SECRET'),
+                       ["bash", "webhooks/setup-update.sh"])
+
+
+@app.route('/webhooks/website-update', methods=['POST'])
+def website_update_webhook():
+    return run_webhook(request,
+                       getenv('WEBSITE_UPDATE_WEBHOOK_SECRET'),
+                       ["bash", "webhooks/setup-update.sh"])
+
+
+@app.route('/webhooks/cv-update', methods=['POST'])
+def cv_update_webhook():
+    return run_webhook(request,
+                       getenv('CV_UPDATE_WEBHOOK_SECRET'),
+                       ["bash", "webhooks/cv-update.sh"])
 
 
 if __name__ == '__main__':
