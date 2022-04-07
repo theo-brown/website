f6b5b0d54a0c58d0dd9173338e92fc98d3940c33
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2022-04-07 20:32:35 +0100

    Fix submodule pulling

--- webhooks/website-update.sh
+++ webhooks/website-update.sh
@@ -3,7 +3,8 @@ systemctl --user stop website
 echo "Discarding local changes..."
 git reset --hard
 echo "Pulling remote changes..."
-git pull
+git submodule init
+git pull --recurse-submodules
 echo "Starting website service..."
 systemctl --user start website
 echo "Done."
